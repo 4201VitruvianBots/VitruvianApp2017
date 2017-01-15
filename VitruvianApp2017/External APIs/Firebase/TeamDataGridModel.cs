9cb5864a9ecc375ee60bcfcbc5e4c98e8b920a07
jonathandao0 <jonathandao0@gmail.com>
2017-01-14 23:13:45 -0800

    Minor changes/additions

---
+++ VitruvianApp2017/External APIs/Firebase/TeamDataGridModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace VitruvianApp2017
+{
+	public class TeamData
+	{
+		public double teamNumber { get; set; }
+		public string teamName { get; set; }
+		public double avgScore { get; set; }
+		public bool imageWrite { get; set; }
+
+		// Pit Data
+
+		// Match Data
+	}
+}
