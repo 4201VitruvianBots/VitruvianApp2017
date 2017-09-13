75dfc7abc06a5cef480b3ccc35935321f461648c
Jonathan Dao <jonathandao0@gmail.com>
2017-09-13 15:20:18 -0700

    Began reorganizing EventMatchData for Tableau

--- VitruvianApp2017/External APIs/Firebase/Models/EventMatchData.cs
+++ VitruvianApp2017/External APIs/Firebase/Models/EventMatchData.cs
@@ -4,12 +4,11 @@ namespace VitruvianApp2017
 	public class EventMatchData
 	{
 		public string matchNumber { get; set; }
-		public int[] Blue { get; set; }
-		public int[] Red { get; set; }
+		//public int[] Blue { get; set; }
+		//public int[] Red { get; set; }
+		public string alliance { get; set; }
+		public int alliancePos { get; set; }
+		public int teamNumber { get; set; }
 		public long matchTime { get; set; }
-
-		// Pit Data
-
-		// Match Data
 	}
 }
